7f47d2e5e64d89f4895cc98cca06b6edf3967f30
Oleksandr Chornyi <chornyi.oleksandr@lll.kpi.ua>
2023-12-01 17:58:30 +0100

    Amend graphql schema and add type resolving

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -1,19 +1,23 @@
+import { isObjWithField } from "../utils/typeguards.js";
+
 const mockedProducts = [
   {
     id: "1",
-    type: "GPS Receiver",
+    type: "GPS_Receiver",
     brand: "Texas Instruments",
     model: "F-235",
+    interfaces: ["USB"],
   },
   {
     id: "2",
-    type: "GPS Receiver",
+    type: "GPS_Receiver",
     brand: "U-Blox",
     model: "gh8372-fn",
+    interfaces: ["USB-C"],
   },
   {
     id: "3",
-    type: "GPS Transmitter",
+    type: "Servo",
     brand: "Hub & Son",
     model: "One",
   },
@@ -23,7 +27,17 @@ interface ProductsOfTypeArgs {
   type: string;
 }
 
+const isTypeOf = (type: string) => (obj: unknown) => {
+  return isObjWithField("type", obj) && obj?.type === type;
+};
+
 export const resolvers = {
+  GPSReceiver: {
+    __isTypeOf: isTypeOf("GPS_Receiver"),
+  },
+  Servo: {
+    __isTypeOf: isTypeOf("Servo"),
+  },
   Query: {
     async products() {
       return mockedProducts;
--- src/graphql/typeDefs.ts
+++ src/graphql/typeDefs.ts
@@ -1,13 +1,37 @@
 export const typeDefs = `#graphql
-  type Product {
+  enum ProductType {
+    GPS_Receiver
+    Servo
+  }
+
+  interface Product {
     id: ID!
-    type: String!
+    type: ProductType!
     brand: String!
     model: String!
+    manufacturerCountry: String
+  }
+
+  type GPSReceiver implements Product {
+    id: ID!
+    type: ProductType!
+    brand: String!
+    model: String!
+    manufacturerCountry: String
+    interfaces: [String!]
+  }
+
+  type Servo implements Product {
+    id: ID!
+    type: ProductType!
+    brand: String!
+    model: String!
+    manufacturerCountry: String
+    power: Float!
   }
 
   type Query {
     products: [Product!]
-    productsOfType(type: String): [Product!]
+    productsOfType(type: ProductType): [Product!]
   }
 `;
